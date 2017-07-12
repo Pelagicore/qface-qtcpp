32c04fcf419236438784c46ef7132ae737df0d93
Juergen Bocklage-Ryannel <jbocklage-ryannel@luxoft.com>
2017-07-12 13:59:18 +0200

    updated setup file

--- qtcpp/qtcpp.py
+++ qtcpp/qtcpp.py
@@ -15,9 +15,10 @@ from qface.watch import monitor
 
 here = Path(__file__).dirname()
 
-logging.config.dictConfig(yaml.load(open(here / 'log.yaml')))
-
-log = logging.getLogger(__file__)
+logging.basicConfig()
+with open('log.yaml', 'r') as fp:
+    logging.config.dictConfig(yaml.load(fp))
+log = logging.getLogger(__name__)
 
 
 def run(src, dst):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qface-qtcpp',
-    version='1.0',
+    version='1.1',
     description='Qt CPP generator based on the QFace library',
     url='https://github.com/Pelagicore/qface-qtcpp',
     author='jryannel',
@@ -15,15 +15,18 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
-    keywords='qt code generator framework',
+    keywords='qt cpp generator',
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        '': ['*.h', '*.cpp', '*.pro', '*.pri', '*.qml', '*.js', '*.j2', '*.qdocconf', '*.conf', 'CMakeLists.txt', 'qmldir']
+    },
     install_requires=[
-        'qface',
+        'qface>=1.3.1',
     ],
     entry_points={
         'console_scripts': [
-            'qtcpp = qtcpp:app'
+            'qface-qtcpp = qtcpp.qtcpp:app'
         ],
     },
 )
